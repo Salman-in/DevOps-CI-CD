de05fcbec4e5e36faa9dcf933a085bf1aa7e6510
Salman <salmannajah123@gmail.com>
2025-02-06 17:41:54 +0530

    prod changed

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
 
   return (
     <div>
-      first name on prod: 
+      first name on production: 
       {user?.username}
       password: 
       {user?.password}
