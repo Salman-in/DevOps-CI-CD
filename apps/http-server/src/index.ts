ecb9f3321f6e2628a3f43ddb9b3cceba1307c03f
Salman <salmannajah123@gmail.com>
2025-02-06 20:42:50 +0530

    HTTP server contents changed:

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hi there");
+    res.send("Salman s HTTP server on production");
 })
 
 app.post("/signup", async (req, res) => {
